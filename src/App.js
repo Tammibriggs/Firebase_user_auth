2e9c7a3ababac072c7bbb7786a43e7db7593b2e2
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-12-15 23:56:02 +0100

    created UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Profile from './Profile'
+import Register from './Register'
+import VerifyEmail from './VerifyEmail';
+import Login from './Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Profile} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path='/verify-email' component={VerifyEmail} /> 
+      </Switch>
+  </Router>
   );
 }
 
---
+++ src/Register.js
@@ -0,0 +1,48 @@
+import {useState} from 'react'
+import { Link } from 'react-router-dom'
+import './forms.css'
+
+function Register() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  return (
+    <div className='center'>
+      <div className='auth'>
+        <h1>Register</h1>
+        <form >
+          <input 
+            type='email' 
+            value={email}
+            placeholder="Enter your email"
+            required
+            onChange={e => setEmail(e.target.value)}/>
+
+          <input 
+            type='password'
+            value={password} 
+            required
+            placeholder='Enter your password'
+            onChange={e => setPassword(e.target.value)}/>
+
+            <input 
+            type='password'
+            value={confirmPassword} 
+            required
+            placeholder='Confirm password'
+            onChange={e => setConfirmPassword(e.target.value)}/>
+
+          <button type='submit'>Register</button>
+        </form>
+        <span>
+          Already have an account?  
+          <Link to='/login'>login</Link>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default Register
