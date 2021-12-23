5b2ef4a1276d41384b12dcca6c3def80b27f5dd2
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-12-23 17:24:28 +0100

    displayed email and email verified value on profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,16 +1,21 @@
 import './profile.css'
+import {useAuthValue} from './AuthContext'
 
 function Profile() {
-  
+  const {currentUser} = useAuthValue()
+
   return (
-    <div className='center'>
-      <div className='profile'>
-        <h1>Profile</h1>
-        <p><strong>Email: </strong> </p>
-        <p><strong>Email verified: </strong> </p>
-        <span>Sign Out</span>
+      <div className='center'>
+        <div className='profile'>
+          <h1>Profile</h1>
+          <p><strong>Email: </strong>{currentUser?.email}</p>
+          <p>
+            <strong>Email verified: </strong>
+            {`${currentUser?.emailVerified}`}
+          </p>
+          <span>Sign Out</span>
+        </div>
       </div>
-    </div>
   )
 }
 
