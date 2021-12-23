50329cbf4b7e7e98e0ae907639858d19212c3b80
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-12-23 17:03:50 +0100

    managing user state with context api

--- src/App.js
+++ src/App.js
@@ -4,16 +4,32 @@ import Profile from './Profile'
 import Register from './Register'
 import VerifyEmail from './VerifyEmail';
 import Login from './Login'
+import {useState, useEffect} from 'react'
+import {AuthProvider} from './AuthContext'
+import {auth} from './firebase'
+import {onAuthStateChanged} from 'firebase/auth'
+
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Profile} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path='/verify-email' component={VerifyEmail} /> 
-      </Switch>
+      <AuthProvider value={{currentUser}}>
+        <Switch>
+          <Route exact path="/" component={Profile} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path='/verify-email' component={VerifyEmail} /> 
+        </Switch>  
+      </AuthProvider>
   </Router>
   );
 }
---
+++ src/AuthContext.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react'
+
+const AuthContext = React.createContext()
+
+export function AuthProvider({children, value}) {
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuthValue(){
+  return useContext(AuthContext)
+}
